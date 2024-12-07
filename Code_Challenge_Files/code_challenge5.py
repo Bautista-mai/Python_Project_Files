469a71950e3890ad1e4409fd46d9f35134b2e24f
Bautista-mai <bautistaalma785@gmail.com>
2024-12-07 23:21:31 +0800

    cc5

---
+++ Code_Challenge_Files/code_challenge5.py
@@ -0,0 +1,5 @@
+print("Farenheit to Celsius Converter Program!")
+fahrenheit = eval(input("Enter temperature in Fahrenheit: "))
+celsius = (fahrenheit - 32) * 5 / 9
+print(f" {fahrenheit}degrees Fahrenheit converter to celsius is {celsius} degrees")
+print(round(celsius, 2))
