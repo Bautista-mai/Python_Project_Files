e9cc36a153781a7f757f593b2e76df8769ddebcd
Bautista-mai <bautistaalma785@gmail.com>
2024-12-08 15:52:14 +0800

    ccc

--- Activity_Files/Activity24.py
+++ Activity_Files/Activity24.py
@@ -1,10 +1,7 @@
 
 from Activity23 import add
-from code_Challenge4 import asteris as ast, num
+import Activity1 
 
 add(21,21)
-num(5) # the docstring
+Activity1
 
-#remove '#' to run the ast() function
-#ast()  
-#should be asteris but since I declare it as 'ast', it must be executed as ast()
