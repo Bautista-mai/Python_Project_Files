1ce1f0962e8cebfc81124e8d28b775dacbbb145f
Bautista-mai <bautistaalma785@gmail.com>
2024-12-09 22:32:16 +0800

    nww cc

---
+++ Code_Challenge_Files/code_challenge10.py
@@ -0,0 +1,28 @@
+#code_challenge10
+
+for h in range(1, 16):
+    for e in range(h,16):
+        print(" ",end=" ")
+        
+    for l in range(1, h + 1):
+        print("*",end=" ")
+        
+    for o in range(1, h + 1):
+        print("*",end=" ")
+    print()
+    
+    
+    
+for h in range(1, 16):
+    for e in range(1, h+1):
+        print(" ",end=" ")
+        
+    for l in range(16, h, -1):
+        print("*",end=" ")
+        
+    for o in range(16, h, -1):
+        print("*",end=" ")
+    print()        
+        
+  
+  
