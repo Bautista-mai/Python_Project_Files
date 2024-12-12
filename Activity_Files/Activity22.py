840a95994cbf11ad0b1adacd0028cc4028e70837
Bautista-mai <bautistaalma785@gmail.com>
2024-12-12 21:37:16 +0800

    act 4

--- Activity_Files/Activity22.py
+++ Activity_Files/Activity22.py
@@ -51,3 +51,25 @@ def act22():
               language+" languages. And my hobbies are "+hobbies+". I'm currently living in "+subd+","+
               street+","+brgy+","+city+","+province+" and you can contact me at", contact," or "+email+
               ". Lastly, I'm currently a college student, I'm enrolled in DLL and my course is BSIT", bsit)
+        
+    def act4():
+        number1  = eval(input("enter a number --> "))
+
+        number2 = eval(input("enter a number --> "))
+
+        answer1 = number1 + number2
+        answer2 = number1 - number2
+        answer3 = number1 * number2
+        answer4 = number1 / number2
+        answer5 = number1 ** number2
+        answer6 = number1 % number2
+        answer7 = number1 // number2
+
+
+        print("The sum of " , number1, " and " , number2, " is " , answer1)
+        print("The difference of" , number1, " and " , number2, " is " , answer2)
+        print("The product of" , number1, " and " , number2, " is " , answer3)
+        print("The quotient of" , number1, " and " , number2, " is " , answer4)
+        print("The exponent of" , number1, " and " , number2, " is " , answer5)
+        print("The remainder of" , number1, " and " , number2, " is " , answer6)
+        print("The floor division of" , number1, " and " , number2, " is " , answer7)
