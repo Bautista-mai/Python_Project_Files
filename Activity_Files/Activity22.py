6ccd6c31e032d1ddeb7d1d36c0410308ead09f4e
Bautista-mai <bautistaalma785@gmail.com>
2024-12-12 21:38:57 +0800

    act5

--- Activity_Files/Activity22.py
+++ Activity_Files/Activity22.py
@@ -73,3 +73,11 @@ def act22():
         print("The exponent of" , number1, " and " , number2, " is " , answer5)
         print("The remainder of" , number1, " and " , number2, " is " , answer6)
         print("The floor division of" , number1, " and " , number2, " is " , answer7)
+
+    def act5():
+        temp = int(input("Enter Temperature in Ferenheit: "))
+
+        cel = (temp - 32) * 5/9
+        round = (round(cel, 2))
+
+        print(f" The convension of {temp} degrees farenheit is {round} degrees celcius. ")
