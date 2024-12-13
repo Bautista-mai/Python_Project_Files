aadeea245a7daba4c0d746ceb2de3a910c776899
Bautista-mai <bautistaalma785@gmail.com>
2024-12-13 23:42:30 +0800

    act22

--- Activity_Files/Activity22.py
+++ Activity_Files/Activity22.py
@@ -97,4 +97,20 @@ def act22():
         x += 30
         print(x)
 
+    def act7():
+        gold = 0
+        min = input("Hi, What is your name: ")
+
+        isgold = input("Is your mineral Gold? [Yes / No]: ").upper().strip()
+
+        if isgold == "YES":
+            gold += 1
+            print(f"Hi {min}, Your total number of gold is: {gold}")
+
+        elif isgold =="NO":
+            print("Please input a gold")
+
+        else:
+            print("Invalid input")    
+
         """Unfinished work"""
