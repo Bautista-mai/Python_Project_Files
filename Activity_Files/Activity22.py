843a6c189b530e6d5ee190858a83de51c9b5ae50
Bautista-mai <bautistaalma785@gmail.com>
2024-12-14 22:30:26 +0800

    act22

--- Activity_Files/Activity22.py
+++ Activity_Files/Activity22.py
@@ -154,6 +154,25 @@ def act22():
         else:
             print("Invalid input")
 
+    def act10():
+        name = input("Enter your name: ")
+        isstudent = input("Are you a student of DLL: ").upper().strip()
 
+        if isstudent == "YES":
+            yearlev = input("What year are you currently enrolled? \nf- Freshman \ns- Sophomore \nsn- senior: \n Please enter your grade level: ").upper().strip()
+
+            if yearlev =="f":
+                print(f"Hi your {name} , freshman, Welcome to DLL")
+
+            elif yearlev == "s":
+                print(f"Hi your {name} , sophomore, Welcome again to DLL")
+                
+            elif yearlev == "sn":
+                print(f"Hi your {name} , senior, Welcome to DLL it's your last year. Goodluck!!")
+
+        else:
+            print("Thank you for using the system")
+
+    
 
         """Unfinished work act 10 to act 25 is still not done"""
