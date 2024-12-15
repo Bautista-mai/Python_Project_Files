b50d7a2225bcd51c43e2568c786e94effed977e2
Bautista-mai <bautistaalma785@gmail.com>
2024-12-15 21:01:42 +0800

    act 19 20

--- Activity_Files/Activity19.py
+++ Activity_Files/Activity19.py
@@ -1,13 +1,14 @@
-isContinue = True
+def act19():
+    isContinue = True
 
-while isContinue:
-    name = input("Give me name: ")
+    while isContinue:
+        name = input("Give me name: ")
 
-    if name.lower() == "stop":
-        print("Loop terminated")
-        break
+        if name.lower() == "stop":
+            print("Loop terminated")
+            break
 
-        isContinue = False
-    else:
-        print(f"Hi {name}")
-        continue
+            isContinue = False
+        else:
+            print(f"Hi {name}")
+            continue
--- Activity_Files/Activity20.py
+++ Activity_Files/Activity20.py
@@ -1,14 +1,15 @@
-continu = True
-count = 0
+def act20():
+    continu = True
+    count = 0
 
-while continu == True:
-    name = input("Enter a name: ").uppr().strip()
+    while continu == True:
+        name = input("Enter a name: ").uppr().strip()
 
-    if name == "STOP":
-        print("The loop has now atopped")
-        print(f"You have entered {count} number of names")
-        break
+        if name == "STOP":
+            print("The loop has now atopped")
+            print(f"You have entered {count} number of names")
+            break
 
-    else:
-        count += 1
-        continue
+        else:
+            count += 1
+            continue
