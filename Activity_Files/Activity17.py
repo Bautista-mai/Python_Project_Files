b6115c7ecdabf403ecd01be174a36891bf97f653
Bautista-mai <bautistaalma785@gmail.com>
2024-12-15 21:01:30 +0800

    act 17 18

--- Activity_Files/Activity17.py
+++ Activity_Files/Activity17.py
@@ -1,19 +1,20 @@
-for x in range(6, 0, -1):
-    for z in range(1, x + 1):
-        print(" ",end=" ")
-    for y in range(6, x, -1):
-        print("*",end=" ")
-    for a in range(5, x, -1):
-        print("*",end=" ")
+def act17():
+    for x in range(6, 0, -1):
+        for z in range(1, x + 1):
+            print(" ",end=" ")
+        for y in range(6, x, -1):
+            print("*",end=" ")
+        for a in range(5, x, -1):
+            print("*",end=" ")
 
-    print()
+        print()
 
-for x in range(1, 6):
-    for z in range(1, x + 2):
-        print(" ",end=" ")
-    for y in range(5, x, -1):
-        print("*",end=" ")
-    for a in range(4, x, -1):
-        print("*",end=" ")
+    for x in range(1, 6):
+        for z in range(1, x + 2):
+            print(" ",end=" ")
+        for y in range(5, x, -1):
+            print("*",end=" ")
+        for a in range(4, x, -1):
+            print("*",end=" ")
 
-    print()
+        print()
--- Activity_Files/Activity18.py
+++ Activity_Files/Activity18.py
@@ -1,11 +1,12 @@
-num = int(input("Enter a number of right tringle: "))
+def act18():
+    num = int(input("Enter a number of right tringle: "))
 
-for i in range(1, 6):
-    for y in range(1, num + 1):
-        print(" ",end=" ")
-        for u in range(1, i + 1):
-            print("*",end=" ")
-        for n in range(5,i, -1):
+    for i in range(1, 6):
+        for y in range(1, num + 1):
             print(" ",end=" ")
+            for u in range(1, i + 1):
+                print("*",end=" ")
+            for n in range(5,i, -1):
+                print(" ",end=" ")
 
-    print()
+        print()
