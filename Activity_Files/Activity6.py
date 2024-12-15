3dfe6a8319acebf90ad72a0bcde6967d37b00019
Bautista-mai <bautistaalma785@gmail.com>
2024-12-15 10:51:35 +0800

    act6

--- Activity_Files/Activity6.py
+++ Activity_Files/Activity6.py
@@ -1,10 +1,11 @@
-x = 5
-print(x)
+def Act6():
+    x = 5
+    print(x)
 
-x += 10
-print(x)
+    x += 10
+    print(x)
 
-x += 15
-print(x)
-x += 30
-print(x)
+    x += 15
+    print(x)
+    x += 30
+    print(x)
