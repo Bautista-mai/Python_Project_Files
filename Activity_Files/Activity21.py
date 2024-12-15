64ee60f402600d06b90bd16bf2455d87421d99f6
Bautista-mai <bautistaalma785@gmail.com>
2024-12-15 21:05:29 +0800

    act 21 23

--- Activity_Files/Activity21.py
+++ Activity_Files/Activity21.py
@@ -1,19 +1,18 @@
+def act21():
+    def cute():
+        print("Hello, you're cute")
 
-
-def cute():
-    print("Hello, you're cute")
-
-def tri():
-    num = int(input("Enter a number of right tringle: "))
-    for i in range(1, 6):
-        for y in range(1, num + 1):
-            print(" ",end=" ")
-            for u in range(1, i + 1):
-                print("*",end=" ")
-            for n in range(5,i, -1):
+    def tri():
+        num = int(input("Enter a number of right tringle: "))
+        for i in range(1, 6):
+            for y in range(1, num + 1):
                 print(" ",end=" ")
-    print()
+                for u in range(1, i + 1):
+                    print("*",end=" ")
+                for n in range(5,i, -1):
+                    print(" ",end=" ")
+        print()
 
-cute()
-tri()
+    cute()
+    tri()
 
