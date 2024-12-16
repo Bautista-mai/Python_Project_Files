ea2a8ae70223b67f657b107ed1ffcd7f42032da6
Bautista-mai <bautistaalma785@gmail.com>
2024-12-16 10:41:03 +0800

    finals

---
+++ Final_Project.py
@@ -0,0 +1,10 @@
+import os
+
+from Activity_Files import ( 
+
+                    Activity1, Activity2, Activity3, Activity4, Activity5, Activity6, Activity7,
+                    Activity8, Activity9, Activity10, Activity11, Activity12, Activity13, Activity14,
+                    Activity15, Activity16, Activity17, Activity18, Activity19, Activity20, Activity21,
+                    Activity22, Activity23, Activity24, Activity25
+
+                    )
