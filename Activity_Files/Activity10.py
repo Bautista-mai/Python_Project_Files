d9acb079b4154a251b6622e8672755367c6e08a8
Bautista-mai <bautistaalma785@gmail.com>
2024-12-16 16:18:35 +0800

    modified

--- Activity_Files/Activity10.py
+++ Activity_Files/Activity10.py
@@ -5,15 +5,16 @@ def Act10():
     if isstudent == "YES":
         yearlev = input("What year are you currently enrolled? \nf- Freshman \ns- Sophomore \nsn- senior: \n Please enter your grade level: ").upper().strip()
 
-        if yearlev =="f":
+        if yearlev =="F":
             print(f"Hi your {name} , freshman, Welcome to DLL")
 
-        elif yearlev == "s":
+        elif yearlev == "S":
             print(f"Hi your {name} , sophomore, Welcome again to DLL")
             
-        elif yearlev == "sn":
+        elif yearlev == "SN":
             print(f"Hi your {name} , senior, Welcome to DLL it's your last year. Goodluck!!")
 
     else:
         print("Thank you for using the system")
 
+Act10()
